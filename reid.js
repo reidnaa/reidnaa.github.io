fd2ecd44d8f0726951e620b92c5198985e326578
reidnaa <reidnaaykens@gmail.com>
2018-01-25 07:10:05 +0000

    fix pdf link

--- reid.js
+++ reid.js
@@ -6,34 +6,39 @@ $('.contact').slideDown(5000);
   $('#who').on('click', function(){
     $('.summary').siblings().slideUp();
     $('.summary').slideToggle('slow').toggleClass('active');
-    $('.cleo-image').fadeOut('slow');
+
   });
 
 
  $('#education').on('click', function(){
    $('.education').siblings().slideUp();
     $('.education').slideToggle('slow');
+    $('.cleo-image').fadeOut('slow');
   });
 
   $('#experience').on('click', function(){
     $('.experience').siblings().slideUp();
     $('.experience').slideToggle('slow');
+    $('.cleo-image').fadeOut('slow');
   });
 
 
   $('#skills').on('click', function(){
     $('.skills').siblings().slideUp();
     $('.skills').slideToggle('slow');
+    $('.cleo-image').fadeOut('slow');
   });
 
   $('#volunteer').on('click', function(){
     $('.volunteer').siblings().slideUp();
     $('.volunteer').slideToggle('slow');
+    $('.cleo-image').fadeOut('slow');
   });
 
   $('#projects').on('click', function(){
     $('.projects').siblings().slideUp();
     $('.projects').slideToggle('slow');
+    $('.cleo-image').fadeOut('slow');
   });
 
   $('.cleo').on('click', function(){
