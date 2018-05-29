0afab20ada302b105ec9ffefafaecbfd25b2b418
reidnaa <reidnaaykens@gamil.com>
2018-05-29 03:33:34 +0000

    scroll to top function

--- reid.js
+++ reid.js
@@ -4,6 +4,24 @@ $(function() {
 
 
 
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 50) {
+            $(this).css('display','absolute');
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+
+
+
 ///////////////
 setTimeout(
   function()
